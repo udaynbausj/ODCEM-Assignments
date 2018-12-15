82b285da046867bf90370851a14e4d62a0b7ebcd
Uday BK <uday.nbausj@gmail.com>
2018-12-15 12:16:14 +0530

    New commit
    Thread Responds for Database Connection and Retrieveal

--- OneDirect/Assaignment4.java
+++ OneDirect/Assaignment4.java
@@ -1,38 +1,65 @@
-package com.example.helloworld;
-
 import java.sql.*;
 
-public class Assaignment2 implements Runnable{
-    public void run(){
-        //Here goes JDBC Code : )
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/sonoo",
-                    "root",
-                    "root"
-            );
-            Statement smt = con.createStatement();
-            ResultSet rs = smt.executeQuery("select * from emp");
-            while(rs.next()){
-                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
+class Mythread1 implements Runnable {
+    String name;
+    Thread t;
 
-            }
-
-            con.close();
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
+    Mythread1(String ThreadName) {
+        name = ThreadName;
+        t = new Thread(this, name);
+        System.out.println("New Thread : " + t.getName());
+        t.start();
     }
 
+    public void run() {
+        try {
+            System.out.println("JDBC CODE HERE ....Handled by ThreadID ");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+                Statement smt = con.createStatement();
+                ResultSet rs = smt.executeQuery("select * from item_details");
+                while (rs.next()) {
+                    System.out
+                            .println(rs.getString(1) + " " + rs.getInt(2) + " " + rs.getInt(3) + " " + rs.getString(4));
+                }
+
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Exception Occured at Database connection and Retrieval " + e);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception Occured : " + e);
+        }
+    }
+}
+
+class Mythread2 implements Runnable {
+    String name;
+    Thread t;
+
+    Mythread2(String ThreadName) {
+        name = ThreadName;
+        t = new Thread(this, name);
+        System.out.println("New Thread : " + t.getName());
+        t.start();
+    }
+
+    public void run() {
+        try {
+            System.out.println("Retrieve from collection code is Handled by ThreadID :");
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Exception Occured : " + e);
+        }
+    }
+}
+
+public class Threads {
     public static void main(String[] args) {
-        Assaignment2 as2 = new Assaignment2();
-        Thread t1 = new Thread(as2);
-
-        System.out.println("The Thread named : "+t1.getName()+" with ID : "+t1.getId()+" started Execution ");
-
-        t1.start();
-
+        new Mythread1("ThreadOne");
+        new Mythread2("ThreadTwo");
+        System.out.println("Main Thread Exiting : ");
     }
 }
