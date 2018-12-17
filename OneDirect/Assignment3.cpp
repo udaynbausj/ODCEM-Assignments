8540cf2bf983d7113aaf1c18f18863e854b53fcc
Uday BK <uday.nbausj@gmail.com>
2018-12-17 12:26:22 +0530

    Architecture of Graph is Tested

---
+++ OneDirect/Assignment3.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <unistd.h>
+using namespace std;
+
+struct bigGraph
+{
+    int id;
+    string name;
+    vector<string> additional_info;
+    vector<int> child_nodes;
+    vector<int> parent_node;
+};
+
+void addedge(map<int, bigGraph> &mp, int source, int dest, bigGraph bg[])
+{
+    bg[source].child_nodes.push_back(dest);
+    bg[dest].parent_node.push_back(source);
+    mp[source] = bg[source];
+    mp[dest] = bg[dest];
+}
+
+void printGraphInfo(map<int, bigGraph> mp)
+{
+    cout << "\nRetrieving the Graph's Information : " << endl;
+    usleep(10000);
+    for (auto itr = mp.begin(); itr != mp.end(); itr++)
+    {
+        cout << "\nNode id  : " << itr->first;
+        bigGraph b = itr->second;
+        cout << "\nRetrieving " << b.name << "'s info : ";
+        cout << "\nName of the Node : " << b.name;
+        cout << "\nImmediate children : ";
+        for (int j = 0; j < b.child_nodes.size(); j++)
+        {
+            cout << b.child_nodes[j] << " ";
+        }
+        cout << "\nParent children : ";
+        for (int j = 0; j < b.parent_node.size(); j++)
+        {
+            cout << b.parent_node[j] << " ";
+        }
+        cout << "\n\n";
+    }
+}
+
+
+void getParent(){
+    
+}
+
+
+
+int main()
+{
+
+    cout << "How many Vertices are you planning ? \n";
+    int vertices, edges, node1, node2, nodeid;
+    string nodename;
+    cin >> vertices;
+    bigGraph bg[10000];
+
+    set<int> registered_nodes;
+
+    cout << "\nEnter 'Node id' , 'Node name' : \n";
+
+    for (int i = 0; i < vertices; i++)
+    {
+        cin >> nodeid >> nodename;
+        registered_nodes.insert(nodeid);
+        bg[nodeid].id = nodeid;
+        bg[nodeid].name = nodename;
+    }
+    map<int, bigGraph> mp;
+    cout << "\nHow many edges are you planning : ?\n";
+    cin >> edges;
+    cout << "\nEnter source node first , dest node second : \n";
+    for (int i = 0; i < edges; i++)
+    {
+        cin >> node1 >> node2;
+
+        if (registered_nodes.find(node1) != registered_nodes.end())
+        {
+
+            addedge(mp, node1, node2, bg);
+        }
+        else
+        {
+            cout << "\nYou have entered Unregistered Node : ";
+        }
+    }
+
+    cout << "\nLets Print Graph Info : " << endl;
+    printGraphInfo(mp);
+}
