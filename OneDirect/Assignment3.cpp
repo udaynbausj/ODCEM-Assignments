54f85aeb74260d0a767ff92d53808bb6fdfe774f
Uday BK <uday.nbausj@gmail.com>
2018-12-17 12:47:27 +0530

    Menu driven Approach is added

--- OneDirect/Assignment3.cpp
+++ OneDirect/Assignment3.cpp
@@ -47,36 +47,41 @@ void printGraphInfo(map<int, bigGraph> mp)
     }
 }
 
-void getImmediateParents(map<int,bigGraph>mp,set<int>s){
+void getImmediateParents(map<int, bigGraph> mp, set<int> s)
+{
     int nodeid;
-    cout<<"\nPlease enter the node id : ";cin>>nodeid;
-    if(s.find(nodeid)!=s.end()){
+    cout << "\nPlease enter the node id : ";
+    cin >> nodeid;
+    if (s.find(nodeid) != s.end())
+    {
         bigGraph b = mp[nodeid];
-        cout<<"\nGetting Immeditate Parent Nodes : )"<<endl;
-        for(int i= 0;i<b.parent_node.size();i++){
-            cout<<b.parent_node[i]<<" ";
+        cout << "\nGetting Immeditate Parent Nodes : )\n" << endl;
+        for (int i = 0; i < b.parent_node.size(); i++)
+        {
+            cout << b.parent_node[i] << " ";
         }
-    }else{
-        cout<<"\nUnregistered Node : ) ";
+    }
+    else
+    {
+        cout << "\nUnregistered Node : ) ";
     }
 }
 
-
-void getImmediateChildren(map<int,bigGraph>mp,set<int>s){
+void getImmediateChildren(map<int, bigGraph> mp, set<int> s)
+{
     int nodeid;
-    cout<<"\nPlease enter the node id : )"<<endl;
-    cin>>nodeid;
-    if(s.find(nodeid)!=s.end()){
+    cout << "\nPlease enter the node id : )" << endl;
+    cin >> nodeid;
+    if (s.find(nodeid) != s.end())
+    {
         bigGraph b = mp[nodeid];
-        cout<<"\nGetting Immediate Children Node : ) "<<endl;
-        for(int i = 0;i<b.child_nodes.size();i++){
-            cout<<b.child_nodes[i]<<" ";
+        cout << "\nGetting Immediate Children Node : ) " << endl;
+        for (int i = 0; i < b.child_nodes.size(); i++)
+        {
+            cout << b.child_nodes[i] << " ";
         }
     }
 }
-
-
-
 
 int main()
 {
@@ -117,6 +122,16 @@ int main()
         }
     }
 
-    cout << "\nLets Print Graph Info : " << endl;
-    printGraphInfo(mp);
+    cout << "please select one of the options : )" << endl;
+
+    cout << "\n1.Get the immediate parents of a node, passing the node id as input parameter."<< "\n2.Get the immediate children of a node, passing the node id as input parameter."<< "\n3.Get the ancestors of a node, passing the node id as input parameter."<< "\n4.Get the descendants of a node, passing the node id as input parameter."<< "\n5.Delete dependency from a tree, passing parent node id and child node id."<< "\n6.Delete a node from a tree, passing node id as input parameter. This should delete all the dependencies of the node."<<"\n7.Add a new dependency to a tree, passing parent node id and child node id. This should check for cyclic dependencies."<<"\n8.Add a new node to tree. This node will have no parents and children. Dependency will be established by calling the 7 number API.";
+    cout<<"\n\n\n";
+    int userchoice;
+    cin>>userchoice;
+    if (userchoice == 1)
+    {
+        getImmediateParents(mp,registered_nodes);
+    }else if(userchoice==2){
+        getImmediateChildren(mp,registered_nodes);
+    }
 }
